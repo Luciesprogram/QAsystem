fc920c1aeb945a96dd85031d59edb74e46b9d20b
Vikrant-Singh <128889858+Luciesprogram@users.noreply.github.com>
2026-01-22 14:44:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="QApplication",
     version="0.0.1",
     author="vikrant-singh",
-    author_email="sunny.savita@gmail.com",
+    author_email="vikrantsingh8709@gmail.com",
     packages=find_packages(),
     py_modules=["exception", "logger"], 
 )
